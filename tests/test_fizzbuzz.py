1dbabab9fb5e008245d0bc741eb7445e4b2a02f0
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-03-06 14:52:04 +0700

    add: Test_Fizzbuzz

---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,23 @@
+import unittest
+from src.fizzbuzz import fizzbuzz
+
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), "Buzz")
+
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+
+    def test_number(self):
+        self.assertEqual(fizzbuzz(7), "7")
+
+    def test_large_number(self):
+        self.assertEqual(fizzbuzz(30), "FizzBuzz")
+
+
+if __name__ == "__main__":
+    unittest.main()
