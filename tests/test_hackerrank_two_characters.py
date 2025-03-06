cde2606a1d488194642e91135e24be2569dc75ec
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-03-06 15:23:44 +0700

    add: Test_two_characters

---
+++ tests/test_hackerrank_two_characters.py
@@ -0,0 +1,23 @@
+import unittest
+from src.hackerrank_two_characters import two_characters
+
+
+class TestTwoCharacters(unittest.TestCase):
+    def test_standard_case(self):
+        self.assertEqual(two_characters("beabeefeab"), 5)
+
+    def test_single_character(self):
+        self.assertEqual(two_characters("aaaa"), 0)
+
+    def test_alternating_characters(self):
+        self.assertEqual(two_characters("ababab"), 6)
+
+    def test_three_characters(self):
+        self.assertEqual(two_characters("abcabcabc"), 4)
+
+    def test_empty_string(self):
+        self.assertEqual(two_characters(""), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
