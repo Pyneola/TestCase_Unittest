e52ba067f4d0131660aac9bd32184c0769269909
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-03-06 15:24:27 +0700

    add: Test_grid_challenge

---
+++ tests/test_hackerrank_grid_challenge.py
@@ -0,0 +1,28 @@
+import unittest
+from src.hackerrank_grid_challenge import grid_challenge
+
+
+class TestGridChallenge(unittest.TestCase):
+    def test_sorted_grid(self):
+        self.assertEqual(grid_challenge(["abc", "def", "ghi"]), "YES")
+
+    def test_unsorted_grid(self):
+        self.assertEqual(grid_challenge(["mpxz", "abcd", "wlmf"]), "NO")
+
+    def test_single_row(self):
+        self.assertEqual(
+            grid_challenge(
+                ["zxcv"],
+            ),
+            "YES",
+        )
+
+    def test_all_same_character(self):
+        self.assertEqual(grid_challenge(["aaaa", "aaaa", "aaaa"]), "YES")
+
+    def test_edge_case_minimum_input(self):
+        self.assertEqual(grid_challenge(["a"]), "YES")
+
+
+if __name__ == "__main__":
+    unittest.main()
