656b74316f71a1dde921b6caa82fe3a7c172c8d8
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-03-06 15:20:56 +0700

    add: Test_funny_string

---
+++ tests/test_hackerrank_funny_string.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from hackerrank_funny_string import funny_string
+
+
+class TestFunnyString(unittest.TestCase):
+    def test_funny_string(self):
+        self.assertEqual(funny_string("acxz"), "Funny")
+
+    def test_not_funny_string(self):
+        self.assertEqual(funny_string("bcxz"), "Not Funny")
+
+    def test_single_character(self):
+        self.assertEqual(funny_string("a"), "Funny")
+
+    def test_palindrome(self):
+        self.assertEqual(funny_string("abba"), "Funny")
+
+    def test_edge_case(self):
+        self.assertEqual(funny_string("az"), "Not Funny")
+
+
+if __name__ == "__main__":
+    unittest.main()
