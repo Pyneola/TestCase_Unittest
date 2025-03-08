c4b52a64f949471b2cd15f0651157b0d3a3a2d70
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-03-08 22:29:37 +0700

    add: coverage testcase with funny_string, grid_challenge, two_characters

--- tests/test_hackerrank_funny_string.py
+++ tests/test_hackerrank_funny_string.py
@@ -9,15 +9,26 @@ from hackerrank_funny_string import funny_string
 class TestFunnyString(unittest.TestCase):
     def test_funny_string(self):
         self.assertEqual(funny_string("acxz"), "Funny")
+        self.assertEqual(funny_string("axbz"), "Funny")
 
     def test_not_funny_string(self):
         self.assertEqual(funny_string("bcxz"), "Not Funny")
+        self.assertEqual(funny_string("abcd"), "Not Funny")
 
     def test_single_character(self):
         self.assertEqual(funny_string("a"), "Funny")
+        self.assertEqual(funny_string("z"), "Funny")
 
     def test_palindrome(self):
         self.assertEqual(funny_string("abba"), "Funny")
+        self.assertEqual(funny_string("racecar"), "Funny")
+
+    def test_edge_case_empty_string(self):
+        self.assertEqual(funny_string(""), "Funny")
+
+    def test_mixed_case(self):
+        self.assertEqual(funny_string("AbBa"), "Not Funny")
+        self.assertEqual(funny_string("aBcXz"), "Not Funny")
 
 
 if __name__ == "__main__":
--- tests/test_hackerrank_grid_challenge.py
+++ tests/test_hackerrank_grid_challenge.py
@@ -10,23 +10,52 @@ from hackerrank_grid_challenge import grid_challenge
 class TestGridChallenge(unittest.TestCase):
     def test_sorted_grid(self):
         self.assertEqual(grid_challenge(["abc", "def", "ghi"]), "YES")
+        self.assertEqual(grid_challenge(["a", "b", "c"]), "YES")
 
     def test_unsorted_grid(self):
         self.assertEqual(grid_challenge(["mpxz", "abcd", "wlmf"]), "NO")
+        self.assertEqual(grid_challenge(["zyx", "wvu", "tsr"]), "NO")
 
     def test_single_row(self):
+        self.assertEqual(grid_challenge(["zxcv"]), "YES")
+        self.assertEqual(grid_challenge(["qwerty"]), "YES")
+
+    def test_all_same_character(self):
+        self.assertEqual(grid_challenge(["aaaa", "aaaa", "aaaa"]), "YES")
+        self.assertEqual(grid_challenge(["bbbb", "bbbb", "bbbb"]), "YES")
+
+    def test_edge_case_minimum_input(self):
+        self.assertEqual(grid_challenge(["a"]), "YES")
+        self.assertEqual(grid_challenge(["z"]), "YES")
+
+    def test_mixed_case(self):
+        self.assertEqual(grid_challenge(["Abc", "Def", "Ghi"]), "NO")
+        self.assertEqual(grid_challenge(["abc", "DEF", "ghi"]), "NO")
+
+    def test_large_grid(self):
         self.assertEqual(
             grid_challenge(
-                ["zxcv"],
+                [
+                    "abcdefgh",
+                    "ijklmnop",
+                    "qrstuvwx",
+                    "yzabcdef",
+                    "ghijklmn",
+                    "opqrstuv",
+                    "wxyzabcd",
+                    "efghijkl",
+                ]
             ),
             "YES",
         )
 
-    def test_all_same_character(self):
-        self.assertEqual(grid_challenge(["aaaa", "aaaa", "aaaa"]), "YES")
+    def test_standard_case(self):
+        self.assertEqual(
+            grid_challenge(["ebacd", "fghij", "olmkn", "trpqs", "xywuv"]), "YES"
+        )
 
-    def test_edge_case_minimum_input(self):
-        self.assertEqual(grid_challenge(["a"]), "YES")
+    def test_large_grid(self):
+        self.assertEqual(grid_challenge(["zzzz", "zzzz", "zzzz", "zzzz"]), "YES")
 
 
 if __name__ == "__main__":
