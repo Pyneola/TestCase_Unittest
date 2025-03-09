6357df8f0f7106841881f7c93f8a6cccc9141cfd
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-03-09 12:49:08 +0700

    fix: test funny string

--- tests/test_hackerrank_funny_string.py
+++ tests/test_hackerrank_funny_string.py
@@ -7,6 +7,12 @@ from hackerrank_funny_string import funny_string
 
 
 class TestFunnyString(unittest.TestCase):
+    def test_funny_string(self):
+        self.assertEqual(funny_string("acxz"), "Funny")
+
+    def test_not_funny_string(self):
+        self.assertEqual(funny_string("bcxz"), "Not Funny")
+
     def test_single_character(self):
         self.assertEqual(funny_string("a"), "Funny")
         self.assertEqual(funny_string("z"), "Funny")
